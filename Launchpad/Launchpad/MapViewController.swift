89eb2c5e8b3b577f38410e3fec1548ad189bf781
greg@4iiii.com <greg@4iiii.com>
2017-11-19 00:35:50 -0700

    Add Calgary to the map. Add function to put whatever lat & long and mark it on the map.

--- Launchpad/Launchpad/MapViewController.swift
+++ Launchpad/Launchpad/MapViewController.swift
@@ -13,19 +13,30 @@ import GoogleMaps
 class MapViewController: UIViewController {
     
     // You don't need to modify the default init(nibName:bundle:) method.
+    var mapView = GMSMapView()
     
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: 51.0486151, longitude: -114.0708459, zoom: 10.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
+/*
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 51.0486151, longitude: -114.0708459)
+        marker.title = "Calgary"
+        marker.snippet = "Alberta"
+        marker.map = mapView
+*/
+    }
+    
+    func addMarker(latitude:Double, longtitude:Double, title:String, snippet:String) {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        marker.title = title
+        marker.snippet = snippet
         marker.map = mapView
     }
 }
